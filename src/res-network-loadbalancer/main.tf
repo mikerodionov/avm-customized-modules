19a926136ffb8727b9ef188fe718d52bbbeb9dac
Mikhail Rodionov <13007691+mikerodionov@users.noreply.github.com>
2025-03-31 18:26:37 +0000

    To add LB module

---
+++ src/res-network-loadbalancer/main.tf
@@ -0,0 +1,32 @@
+module "res-network-loadbalancer" {
+  source = "git::https://github.com/mikerodionov/terraform-azurerm-avm-res-network-loadbalancer.git?ref=v0.4.0"
+
+  # Variables to pass to the child module
+  # main.tf
+  location                      	= var.location
+  name                          	= var.name
+  resource_group_name           	= var.resource_group_name
+  edge_zone                     	= var.edge_zone
+  sku                           	= var.sku
+  sku_tier                      	= var.sku_tier
+  tags                          	= var.tags
+  frontend_ip_configurations    	= var.frontend_ip_configurations
+  frontend_subnet_resource_id   	= var.frontend_subnet_resource_id
+  backend_address_pools         	= var.backend_address_pools
+  backend_address_pool_configuration = var.backend_address_pool_configuration
+  backend_address_pool_addresses	= var.backend_address_pool_addresses
+  backend_address_pool_network_interfaces = var.backend_address_pool_network_interfaces
+  lb_probes                     	= var.lb_probes
+  lb_rules                      	= var.lb_rules
+  lb_nat_rules                  	= var.lb_nat_rules
+  lb_outbound_rules             	= var.lb_outbound_rules
+  lb_nat_pools                  	= var.lb_nat_pools
+  # main.locks.tf
+  lock = var.lock
+  # main.public_ip.tf
+  public_ip_address_configuration=var.public_ip_address_configuration
+  # main.role_assignments.tf
+  role_assignments = var.role_assignments
+  # main.diagnostic_settings.tf
+  diagnostic_settings = var.diagnostic_settings
+}
---
+++ src/res-network-loadbalancer/outputs.tf
@@ -0,0 +1,35 @@
+# Exact copy from child module to ensure feature parity
+output "azurerm_lb" {
+  description = "Outputs the entire Azure Load Balancer resource"
+  value       = azurerm_lb.this
+}
+
+output "azurerm_lb_backend_address_pool" {
+  description = "Outputs each backend address pool in its entirety"
+  value       = azurerm_lb_backend_address_pool.this
+}
+
+output "azurerm_lb_nat_rule" {
+  description = "Outputs each NAT rule in its entirety"
+  value       = azurerm_lb_nat_rule.this
+}
+
+output "azurerm_public_ip" {
+  description = "Outputs each Public IP Address resource in its entirety"
+  value       = azurerm_public_ip.this
+}
+
+output "name" {
+  description = "Outputs the entire Azure Load Balancer resource"
+  value       = azurerm_lb.this.name
+}
+
+output "resource" {
+  description = "Outputs the entire Azure Load Balancer resource"
+  value       = azurerm_lb.this
+}
+
+output "resource_id" {
+  description = "Outputs the entire Azure Load Balancer resource"
+  value       = azurerm_lb.this.id
+}
